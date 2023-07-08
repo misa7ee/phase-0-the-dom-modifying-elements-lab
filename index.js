6feb5c0d5bfee25be9b873cb5a00850ea56d10c3
Sajiya <misa7ee@gmail.com>
2023-07-08 10:06:31 -0400

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+document.querySelector("main#main").remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "SAJIYA-MILI is the champion"
+
+
+
+// The tests that say point to a node usually means that it wants you to create that node.
+// has a 'newHeader' variable that points to an <h1> node
+
+// so we want to use createElement here
+
+    
+
